8f3c04f65128945dcdff9e401e14ccd133cab3ef
Juri Wolf <Amerlander@users.noreply.github.com>
2024-01-24 17:24:36 +0100

    add dynamic routing

--- src/lib/menues/SidebarMenue.svelte
+++ src/lib/menues/SidebarMenue.svelte
@@ -1,11 +1,26 @@
+<script>
+  import { page } from '$app/stores';
+
+  let pages = [
+    {
+      name: "Home",
+      slug: "/"
+    },
+    {
+      name: "Seite 2",
+      slug: "/seite2"
+    },
+    {
+      name: "Mathe",
+      slug: "/mathe"
+    }
+  ]
+</script>
+
 <div class="ui secondary vertical pointing menu">
-  <a class="item">
-    Home
-  </a>
-  <a class="item active">
-    Messages
-  </a>
-  <a class="item">
-    Friends
-  </a>
+  {#each pages as seite} 
+    <a class="item" href="{seite.slug}" class:active={$page.url.pathname == seite.slug}>
+      {seite.name}
+    </a>
+  {/each}
 </div>
---
+++ src/routes/[fach]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { page } from '$app/stores';
+  
+    console.log($page)
+</script>
+
+<h1>
+    {$page.params.fach}
+</h1>

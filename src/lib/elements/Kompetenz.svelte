bd5da0ace22b2861f84f0046673f3cb28f838b35
Juri Wolf <Amerlander@users.noreply.github.com>
2024-02-21 16:52:47 +0100

    Add Pocketbase API

---
+++ src/lib/elements/Kompetenz.svelte
@@ -0,0 +1,50 @@
+<div class="ui fluid cards">
+    <div class="ui fluid card">
+      <div class="content">
+        <div class="right floated">
+            <div class="ui buttons">
+                <div class="ui basic red button">Kennengelernt</div>
+                <div class="ui basic orange button">Geübt</div>
+                <div class="ui basic green button">Beherscht</div>
+              </div>
+        </div>
+        <!-- <img class="right floated mini ui image" src="/images/avatar/large/elliot.jpg"> -->
+        <div class="header">
+          Ich kann ganze Zahlen darstellen
+        </div>
+        <div class="meta">
+          L1, L2
+        </div>
+        <div class="description">
+            <p>Beschreibenden Text (Markdown?)</p>
+        </div>
+        
+        <div class="ui cards">
+            {#each ['Arbeitsblatt (pdf/.docx)','Video (mp4)','Bild'] as document}
+            <div class="card">
+              <div class="content">
+                <i class="right floated check icon"></i>
+                <i class="right floated eye icon"></i>
+                <div class="header">Addition</div>
+                <div class="meta">
+                    <span class="category">{document}</span>
+                  </div>
+                <div class="description">
+                  Beschreibung
+                </div>
+              </div>
+              <a class="ui button" href="#" target="_blank">
+                <i class="download icon"></i>
+                Herunterladen
+              </a>
+            </div>
+            {/each}
+          </div>
+
+
+      </div>
+      <div class="extra content">
+        Optional Fotos/Dateien hochladen zur persöhnlichen Dokumentation
+      </div>
+    </div>
+  </div>
--- src/routes/[fach]/+page.svelte
+++ src/routes/[fach]/+page.svelte
@@ -1,68 +1,47 @@
 <script>
     import { page } from '$app/stores';
+    import PocketBase from 'pocketbase';
+    import Kompetenz from '$lib/elements/Kompetenz.svelte';
 
-    console.log($page)
+    let faecher = [];
+
+    console.log("PAGE", $page)
+
+  
+      const url = 'https://sab.pockethost.io/'
+      const pb = new PocketBase(url)
+
+      async function getFaecher() {
+        faecher = await pb.collection('faecher').getFullList({
+            sort: '-created',
+            filter: `slug = "${$page.params.fach}"`,
+            expand: "themen(fach)"
+        });
+        console.log('faecher', faecher);
+      }
+
+      getFaecher();
+      
 </script>
 
-<h1>
-    Titel: {$page.params.fach}
-</h1>
+{#each faecher as fach}
 
-<p>Beschreibung: ... lorem</p>
+  <h2>
+    {fach.name}
+  </h2>
 
+  {@html fach.beschreibung}
 
-Gruppenname: Ganze Zahlen
+  {#each fach.expand["themen(fach)"] as thema}
+    <h3>{thema.name}</h3>
 
-<div class="ui fluid cards">
-    <div class="ui fluid card">
-      <div class="content">
-        <div class="right floated">
-            <div class="ui buttons">
-                <div class="ui basic red button">Kennengelernt</div>
-                <div class="ui basic orange button">Geübt</div>
-                <div class="ui basic green button">Beherscht</div>
-              </div>
-        </div>
-        <!-- <img class="right floated mini ui image" src="/images/avatar/large/elliot.jpg"> -->
-        <div class="header">
-          Ich kann ganze Zahlen darstellen
-        </div>
-        <div class="meta">
-          L1, L2
-        </div>
-        <div class="description">
-            <p>Beschreibenden Text (Markdown?)</p>
-        </div>
-        
-        <div class="ui cards">
-            {#each ['Arbeitsblatt (pdf/.docx)','Video (mp4)','Bild'] as document}
-            <div class="card">
-              <div class="content">
-                <i class="right floated check icon"></i>
-                <i class="right floated eye icon"></i>
-                <div class="header">Addition</div>
-                <div class="meta">
-                    <span class="category">{document}</span>
-                  </div>
-                <div class="description">
-                  Beschreibung
-                </div>
-              </div>
-              <a class="ui button" href="#" target="_blank">
-                <i class="download icon"></i>
-                Herunterladen
-              </a>
-            </div>
-            {/each}
-          </div>
+    <Kompetenz />
+
+  {/each}
+
+{/each}
 
 
-      </div>
-      <div class="extra content">
-        Optional Fotos/Dateien hochladen zur persöhnlichen Dokumentation
-      </div>
-    </div>
-  </div>
 
 
 
